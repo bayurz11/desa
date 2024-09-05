0dac92efc01a4a900ddf2f128e489db88681f9c8
Bayu <bayurz1001@gmail.com>
2024-09-05 20:52:32 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,6 +63,7 @@ export default function TypewriterEffectDemo() {
       </div>
       <SambutanPageSection />
       <StrukturOrganisasi />
+      <SambutanPageSection />
       {showScrollToTop && (
         <button
           onClick={scrollToTop}
