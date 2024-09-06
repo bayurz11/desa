6881ec253dc89f88f9b25017cac242d6baa207b9
Bayu <bayurz1001@gmail.com>
2024-09-06 15:19:29 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,6 @@ export default function TypewriterEffectDemo() {
       </div>
       <SambutanPageSection />
       <StrukturOrganisasi />
-      <StrukturOrganisasi />
       
       {showScrollToTop && (
         <button
