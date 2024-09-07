8dcfd34bd7e7630c103366e08dee29b16de183bd
Bayu <bayurz1001@gmail.com>
2024-09-07 12:29:02 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "../components/NavBar";
 import { TypewriterEffect } from "../components/ui/typewriter-effect";
 import InfoCards from "../components/InfoCards";
@@ -12,13 +12,21 @@ import Image from "next/image";
 export default function TypewriterEffectDemo() {
   const [isOpen, setIsOpen] = useState(false);
   const [showScrollToTop, setShowScrollToTop] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
 
   const handleScroll = () => {
-    if (window.scrollY > 300) { // Tampilkan tombol jika scroll lebih dari 300px
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight;
+    const winHeight = window.innerHeight;
+    const scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
+
+    if (scrollTop > 200) { // Tampilkan tombol jika scroll lebih dari 300px
       setShowScrollToTop(true);
     } else {
       setShowScrollToTop(false);
     }
+
+    setScrollProgress(scrollPercent);
   };
 
   const scrollToTop = () => {
@@ -68,12 +76,21 @@ export default function TypewriterEffectDemo() {
       <StrukturOrganisasi />
       <PopulationProps total={1234} male={600} female={634} keluarga={650} Sementara={25} Mutasi={2} />
       {showScrollToTop && (
-        <button
-          onClick={scrollToTop}
-          className="fixed bottom-4 right-4 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition-colors"
-        >
-          ↑
-        </button>
+        <div className="fixed bottom-4 right-4 w-16 h-16 flex items-center justify-center">
+          <div
+            className="w-full h-full rounded-full border-4 border-white-500 flex items-center justify-center"
+            style={{
+              background: `conic-gradient(#4F46E5 ${scrollProgress}%, #0000 ${scrollProgress}% 100%)`,
+            }}
+          >
+            <button
+              onClick={scrollToTop}
+              className="bg-blue-500 text-white w-12 h-12 rounded-full flex items-center justify-center shadow-lg hover:bg-blue-700 transition-transform transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
+            >
+              <span className="text-xl">✈️</span>
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
